2679898631ac0c5132704c02ace6aefbf114ee9b
Joseph Casabona <josephcasabona@Josephs-MacBook-Pro.local>
2018-01-10 14:15:18 -0500

    SOme minor changes

--- header-podcatcher.php
+++ header-podcatcher.php
@@ -2,13 +2,22 @@
 
 <div class="latest-episode clear">
 	<h5>Latest Episode</h5>
-	<h3><?php echo esc_html( get_the_title( $latest_episode_id ) ); ?></h3>
+	<div class="featured-image">
+		<?php
+			if ( has_post_thumbnail( $latest_episode_id ) ) {
+				$title = get_the_title( $latest_episode_id );
+				the_post_thumbnail( 'full', $latest_episode_id, array( 'title' => $title ) );
+			}
+		?>
+	</div>
 	<div class="episode">
 		<?php
 			$episode_data = powerpress_get_enclosure_data( $latest_episode_id );
 			echo do_shortcode( '[audio src="'. esc_url( $episode_data['url'] ) .'"]' );
 		?>
 	</div>
-	<p class="alignleft"><a class="show-notes" href="<?php echo get_the_permalink( $latest_episode_id ); ?>">View Show Notes</a></p>
-	<p class="alignright">Sponsored by: <?php echo parsec_sponsor_text_links( $latest_episode_id ); ?></p>
+	<div class="show-links">
+		<p><a class="show-notes" href="<?php echo get_the_permalink( $latest_episode_id ); ?>">View Show Notes</a></p>
+		<p>Sponsored by: <?php echo parsec_sponsor_text_links( $latest_episode_id ); ?></p>
+	</div>
 </div>
